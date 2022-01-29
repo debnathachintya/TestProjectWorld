919f876ebefba01341871fda702214961b6a344f
debnathachintya <achintya.k.debnath@gmail.com>
2022-01-29 19:25:01 +0530

    second

---
+++ src/test/java/testCases/TestCase1.java
@@ -0,0 +1,21 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@Test
+	public void firstTest() {
+		System.out.println("First Test");
+	}
+	
+	@Test
+	public void secondTest() {
+		System.out.println("Second Test");
+	}
+	
+	@Test
+	public void thirdTest() {
+		System.out.println("Third Test");
+	}
+}
